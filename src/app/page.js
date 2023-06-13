5e389e49ca24f8ebd216b9abbee7eea87b15a236
Marta Castrillo <mart.castrillo@gmail.com>
2023-06-13 22:54:56 +0200

    add files

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,39 @@ import styles from "./page.module.scss";
 export default function Home() {
 	const [dataArray, setDataArray] = useState([]);
 	const [inputValues, setInputValues] = useState({});
+let result = 0;
 
+const convertirNumeros = (array) => {
+  const newArray = array.map((element) => {
+    if (!isNaN(element)) {
+      return Number(element);
+    } else {
+      return element;
+    }
+  });
+  console.log('eee');
+  console.log(newArray);
+  return newArray;
+}
+const checkAllNumbers = (array) => {
+  if(convertirNumeros(array)){
+  
+    return array.every((element) => typeof element === 'number');
+  }
+  
+}
+const addArray = (array) => {
+  if(checkAllNumbers(array)){
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+ 
+
+
+}
 	const handleChange = (event) => {
 		const { name, value } = event.target;
 		setInputValues((prevInputValues) => ({
@@ -15,25 +47,30 @@ export default function Home() {
 		}));
 	};
 
-
+  result = addArray(dataArray);
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		const valuesArray = Object.values(inputValues);
 		setDataArray((prevDataArray) => [...prevDataArray, ...valuesArray]);
-		setInputValues({});
-		
+    console.log(dataArray);
+    result = addArray(dataArray);
+    setInputValues({});
 	};
 	const handleReset = (event) => {
 		event.preventDefault();
 		setDataArray([]);
 		setInputValues({});
-	
 	};
+
+
+
+
+
 	return (
-		<div className="superbox">
-			<h1 className="superbox__h1">full stack test</h1>
-			<form action="" className="superbox__form" onSubmit={handleSubmit}>
-				<fieldset className="superbox__form--fieldset">
+		<div className={styles.superbox}>
+			<h1 className={styles.superbox__h1}>full stack test</h1>
+			<form action="" className={styles.superbox__h1} onSubmit={handleSubmit}>
+      <fieldset className={styles.superbox__form__fieldset}>
 					<label htmlFor="value1">value 1</label>
 					<input
 						id="value1"
@@ -43,7 +80,7 @@ export default function Home() {
 						onChange={handleChange}
 					/>
 				</fieldset>
-				<fieldset className="superbox__form--fieldset">
+        <fieldset className={styles.superbox__form__fieldset}>
 					<label htmlFor="value2">value 2</label>
 					<input
 						id="value2"
@@ -53,7 +90,7 @@ export default function Home() {
 						onChange={handleChange}
 					/>
 				</fieldset>
-				<fieldset className="superbox__form--fieldset">
+        <fieldset className={styles.superbox__form__fieldset}>
 					<label htmlFor="value3">value 3</label>
 					<input
 						type="text"
@@ -64,7 +101,7 @@ export default function Home() {
 						onChange={handleChange}
 					/>
 				</fieldset>
-				<fieldset className="superbox__form--fieldset">
+        <fieldset className={styles.superbox__form__fieldset}>
 					<label htmlFor="">value 4</label>
 					<input
 						type="text"
@@ -75,15 +112,15 @@ export default function Home() {
 						onChange={handleChange}
 					/>
 				</fieldset>
-				<div className="extradiv">
+				<div 	className={styles.superbox__form__extradiv}>
 					<input
-						className="superbox__form--submit"
+						className={styles.superbox__form__submit}
 						type="submit"
 						value="Submit"
 					/>
 					<button
 						type="reset"
-						className="superbox__form--submit"
+            className={styles.superbox__form__submit}
 						onClick={handleReset}
 					>
 						Reset
@@ -91,9 +128,8 @@ export default function Home() {
 				</div>
 			</form>
 
-			<div className="superbox__results">
-				<h2>history results:</h2>
-				<h3>resultado</h3>
+			<div className={styles.superbox__results}>
+				<h2>history results: {result}</h2>				
 			</div>
 		</div>
 	);
